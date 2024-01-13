ab6741abba3860f3fb403fe9208e6401068d229c
Moemen99 <117722814+Moemen99@users.noreply.github.com>
2024-01-13 16:21:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ btns.forEach((btn) => {
     btn.classList.add("active");
     const tabContents = document.querySelectorAll(".tab-content");
 
-    // tabContents.forEach((tabContent) => {
-    //   tabContent.className = tabContent.className.replace("active", "");
-    // });
+     tabContents.forEach((tabContent) => {
+       tabContent.className = tabContent.className.replace("active", "");
+     });
 
     tabContents.forEach((tab) => tab.classList.remove("active"));
 
@@ -18,30 +18,30 @@ btns.forEach((btn) => {
 });
 
 
-// document.onkeydown = (e) => {
-//   if (e.key == 123) {
-//       e.preventDefault();
-//   }
-//   if (e.ctrlKey && e.shiftKey && e.key == 'I') {
-//       e.preventDefault();
-//   }
-//   if (e.ctrlKey && e.shiftKey && e.key == 'C') {
-//       e.preventDefault();
-//   }
-//   if (e.ctrlKey && e.shiftKey && e.key == 'J') {
-//       e.preventDefault();
-//   }
-//   if (e.ctrlKey && e.key == 'U') {
-//       e.preventDefault();
-//   }
-// };
+ document.onkeydown = (e) => {
+   if (e.key == 123) {
+      e.preventDefault();
+   }
+   if (e.ctrlKey && e.shiftKey && e.key == 'I') {
+      e.preventDefault();
+  }
+   if (e.ctrlKey && e.shiftKey && e.key == 'C') {
+       e.preventDefault();
+   }
+  if (e.ctrlKey && e.shiftKey && e.key == 'J') {
+       e.preventDefault();
+   }
+   if (e.ctrlKey && e.key == 'U') {
+       e.preventDefault();
+   }
+ };
 
 
-// $(document).ready(function() {
-//   $('body').bind('cut copy paste', function(event) {
-//   event.preventDefault();
-//   });
-//   });
+ $(document).ready(function() {
+   $('body').bind('cut copy paste', function(event) {
+   event.preventDefault();
+   });
+   });
 
-// document.oncontextmenu = new Function("return false");
+ document.oncontextmenu = new Function("return false");
 
